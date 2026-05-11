387a1d385934bc489f842c4e7fe47b1d84fd6a82
Atticus-Yi <19807323866@163.com>
2026-05-11 20:48:26 +0800

    BaseCachePolicy

--- TXCachePolicy.hpp
+++ TXCachePolicy.hpp
@@ -1,12 +1,18 @@
-template <typename Key, typename Value>
-class BasePolicy {
-public:
-    virtual ~BasePolicy() = default;
+#pragma once
 
-    // 核心增删改查接口
+namespace TXCache
+{
+
+template <typename Key, typename Value>
+class TXCachePolicy
+{
+public:
+    virtual ~TXCachePolicy(){};
+
     virtual void put(Key key, Value value) = 0;
     virtual bool get(Key key, Value& value) = 0;
     virtual Value get(Key key) = 0;
-    virtual void remove(Key key) = 0;
-
 };
+
+            
+}
